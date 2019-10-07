4dc619fcdc6d0f5105420370d655c5238c89868e
gramasaurous <grahamgreving@gmail.com>
2019-10-07 17:17:03 +0000

    nicer layout and other improvements

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 docker rm -f baywatch
 
 docker run --name baywatch \
+	   -v /opt/mediaserver/baywatch/archive/:/var/www/html/archive/ \
 	   -p 8008:80 \
 	   --restart=always \
 	   -d baywatch:latest
--- scripts/gif.sh
+++ scripts/gif.sh
@@ -12,14 +12,22 @@ pushd ${FOLDER}
 
 WORKDIR=$(mktemp -d)
 
+mkdir -p ${WORKDIR}/rejects/
+
 mv *.jpg ${WORKDIR}
+mv rejects/*.jpg ${WORKDIR}/rejects/
 
 pushd ${WORKDIR}
 
 rm tmp.jpg
 
-cat *.jpg | ffmpeg -f image2pipe -vcodec mjpeg -i - latest.mp4
+cat *.jpg | ffmpeg -f image2pipe -vcodec mjpeg -i - clean.mp4
+cat rejects/*.jpg | ffmpeg -f image2pipe -vcodec mjpeg -i - reject.mp4
 
-mv latest.mp4 /var/www/html/latest.mp4
+TIMESTAMP=$(date +"%y-%m-%dT%H:%M")
+
+mv rejects.mp4 /var/www/html/archive/rejects/${TIMESTAMP}.mp4
+mv clean.mp4 /var/www/html/archive/${TIMESTAMP}.mp4
+cp /var/www/html/achive/${TIMESTAMP}.mp4 /var/www/html/latest.mp4
 
 
