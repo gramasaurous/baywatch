b5bc309fc89f50bfbb74a488c98e043471d6cf23
gramasaurous <grahamgreving@gmail.com>
2021-11-07 18:53:54 +0000

    fix gif script not handling reject.mp4 filename
    also dont log gunicorn requests

--- bwapp/gunicorn.sh
+++ bwapp/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-SCRIPT_NAME=/baywatch gunicorn --chdir /app baywatch:app -w 2 --threads 2 -b 0.0.0.0:8008 --access-logfile -
+SCRIPT_NAME=/baywatch gunicorn --chdir /app baywatch:app -w 2 --threads 2 -b 0.0.0.0:8008
--- scripts/gif.sh
+++ scripts/gif.sh
@@ -21,14 +21,15 @@ mv rejects/*.jpg ${WORKDIR}/rejects/
 pushd ${WORKDIR}
 
 rm tmp.jpg
+rm clean.mp4
+rm reject.mp4
 
 ffmpeg -f image2 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -vcodec libx264 clean.mp4
-ffmpeg -f image2 -pattern_type glob -i 'rejects/*.jpg' -pix_fmt yuv420p -vcodec libx264 clean.mp4
+ffmpeg -f image2 -pattern_type glob -i 'rejects/*.jpg' -pix_fmt yuv420p -vcodec libx264 reject.mp4
 
 TIMESTAMP=$(date +"%y-%m-%dT%H:%M")
 
 mv reject.mp4 /var/www/html/archive/rejects/${TIMESTAMP}.mp4
 mv clean.mp4 /var/www/html/archive/${TIMESTAMP}.mp4
-cp /var/www/html/archive/${TIMESTAMP}.mp4 /var/www/html/latest.mp4
-
+cp /var/www/html/archive/${TIMESTAMP}.mp4 /var/www/html/archive/latest.mp4
 
